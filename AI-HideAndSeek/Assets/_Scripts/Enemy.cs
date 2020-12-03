0697269020735a3b9371313a44753630fde83acc
Nguyen Gia Huy <nghuycode@gmail.com>
2020-12-04 00:28:12 +0700

    Player + Enemy: move in 8 directions

--- AI-HideAndSeek/Assets/_Scripts/Enemy.cs
+++ AI-HideAndSeek/Assets/_Scripts/Enemy.cs
@@ -7,8 +7,8 @@ public class Enemy : MonoBehaviour
     public int Row, RowToGo, Column, ColumnToGo;
     public MapGenerator MapGenerator;
     public Animator Anim;
-    private void Start() {
-
+    public enum Direction {
+        LEFT, RIGHT, UP, DOWN, LEFTUP, LEFTDOWN, RIGHTUP, RIGHTDOWN
     }
     public void InitRC(int r, int c) {
         Row = r;
@@ -31,6 +31,70 @@ public class Enemy : MonoBehaviour
         if (Input.GetKeyUp(KeyCode.S)) {
             this.transform.eulerAngles = new Vector3(0, 90, 0);
             StartCoroutine(Move(MapGenerator.GetPositionByRowColumn(++Row, Column)));
+        }
+    }
+    public void DecideMove(int newRow, int newColumn) 
+    {
+        if (newRow != Row && newColumn != Column) 
+        {
+            if (newRow > Row && newColumn > Column)
+                Move(Direction.RIGHTDOWN);
+            else if (newRow > Row && newColumn < Column)
+                Move(Direction.LEFTDOWN);
+            else if (newRow < Row && newColumn > Column)
+                Move(Direction.RIGHTUP);
+            else    
+                Move(Direction.LEFTUP);
+        }
+        if (newRow == Row) 
+        {   
+            if (newColumn > Column)
+                Move(Direction.RIGHT);
+            else    
+                Move(Direction.LEFT);
+        }
+        else if (newColumn != Column) 
+        {
+            if (newRow > Row)
+                Move(Direction.DOWN);
+            else
+                Move(Direction.UP);
+        }
+    }
+    public void Move(Direction dir) {
+        switch (dir) {
+            case Direction.LEFT:
+                this.transform.eulerAngles = new Vector3(0, 180, 0);
+                StartCoroutine(Move(MapGenerator.GetPositionByRowColumn(Row, --Column)));
+                break;
+            case Direction.RIGHT:
+                this.transform.eulerAngles = new Vector3(0, 0, 0);
+                StartCoroutine(Move(MapGenerator.GetPositionByRowColumn(Row, ++Column)));
+                break;
+            case Direction.UP:
+                this.transform.eulerAngles = new Vector3(0, -90, 0);
+                StartCoroutine(Move(MapGenerator.GetPositionByRowColumn(--Row, Column)));
+                break;
+            case Direction.DOWN:
+                this.transform.eulerAngles = new Vector3(0, 90, 0);
+                StartCoroutine(Move(MapGenerator.GetPositionByRowColumn(++Row, Column)));
+                break;
+            case Direction.LEFTDOWN:
+                this.transform.eulerAngles = new Vector3(0, 135, 0);
+                StartCoroutine(Move(MapGenerator.GetPositionByRowColumn(--Row, Column)));
+                break;
+            case Direction.LEFTUP:
+                this.transform.eulerAngles = new Vector3(0, -135, 0);
+                StartCoroutine(Move(MapGenerator.GetPositionByRowColumn(--Row, Column)));
+                break;
+            case Direction.RIGHTDOWN:
+                this.transform.eulerAngles = new Vector3(0, 45, 0);
+                StartCoroutine(Move(MapGenerator.GetPositionByRowColumn(--Row, Column)));
+                break;
+            case Direction.RIGHTUP:
+                this.transform.eulerAngles = new Vector3(0, -45, 0);
+                StartCoroutine(Move(MapGenerator.GetPositionByRowColumn(--Row, Column)));
+                break;
         }
     }
     private IEnumerator Move(Vector3 targetPosition) {
